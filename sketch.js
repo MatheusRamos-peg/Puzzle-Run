39e12dfeb839e1d955f37fdb0c179797af1efcbf
Matheus <matferrao2013@gmail.com>
2026-04-08 20:05:57 -0400

    Initialize canvas and background image
    Added background image and setup for canvas.

--- sketch.js
+++ sketch.js
@@ -1,12 +1,21 @@
+var bg, bgImg;
+var topPlayerImg, bottomPlayerImg;
+var lever, leverImg;
+var button, buttonImg; 
 
-
+function preload() {
+   bgImg = loadImage("assets/bg.avif");
+}
 
 function setup() {
-  
+  createCanvas(windowWidth,600);
+ bg = createSprite(1000,300);
+ bg.addImage(bgImg);
+ bg.scale = 1;
 }
 
 function draw() {
-
+background(120);
   
   drawSprites();
 }
