d0b2eda9f3d5a22f600178b072c4b21ace980e58
JPoficial4 <joao.andrade.g4@gmail.com>
2026-04-20 20:17:22 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,25 +1,39 @@
 var bg, bgImg;
-var topPlayerImg, bottomPlayerImg;
+var topPlayer,topPlayerImg,topPlayer,topoPlayeRunning,bottomPlayerImg,bottomPlayerRunning;
 var lever, leverImg;
 var button, buttonImg; 
 
 function preload() {
    bgImg = loadImage("assets/bg.avif");
+   topPlayer = loadImage("assets/esqueleto1.png")  
+   bottomPlayer = loadImage("assets/esqueletobaixo1.png")
+   topPlayerRunning = loadAnimation("assets/esqueleto1.png",
+    "assets/esqueleto2.png",
+    "assets/esqueleto3.png");
+  bottomPlayerRunning = loadAnimation("assets/esqueletobaixo1.png",
+    "assets/esqueletobaixo2.png",
+    "assets/esqueletobaixo3.png");
+
 }
 
 function setup() {
-  createCanvas(windowWidth,600);
- bg = createSprite(1000,300);
+  createCanvas(windowWidth,windowHeight);
+ bg = createSprite(windowWidth/2,windowHeight/2);
  bg.addImage(bgImg);
- bg.scale = 1;
+ bg.scale = 3.2;
+ topPlayer = createSprite(600,500);
+ topPlayer.addAnimation("Running",topPlayerRunning);
+ bottomPlayer = createSprite(400,500);
+ bottomPlayer.addAnimation("Running",bottomPlayerRunning);
 }
 
 function draw() {
 background(120);
-  
+  if(keyDown("x")){
+    topPlayer.changeAnimation("Running");
+    bottomPlayer.changeAnimation("Running");
+  }
   drawSprites();
 }
-
-
 
 
