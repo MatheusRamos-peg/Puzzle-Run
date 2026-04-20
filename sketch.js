bc8e561d8557c34333fa8f945c7ffe4e0a6e20e0
Carla Vasconcelos <88123492+VasconcelosCarla@users.noreply.github.com>
2026-04-20 20:29:01 -0300

    Refactor player animations to include idle state

--- sketch.js
+++ sketch.js
@@ -1,16 +1,17 @@
 var bg, bgImg;
-var topPlayer,topPlayerImg,topPlayer,topoPlayeRunning,bottomPlayerImg,bottomPlayerRunning;
+var topPlayer,bottomPlayer;
+var topPlayerIdle, bottomPlayerIdle,topPlayerImg,topPlayer,topPlayerRunning,bottomPlayerImg,bottomPlayerRunning;
 var lever, leverImg;
 var button, buttonImg; 
 
 function preload() {
    bgImg = loadImage("assets/bg.avif");
-   topPlayer = loadImage("assets/esqueleto1.png")  
-   bottomPlayer = loadImage("assets/esqueletobaixo1.png")
+   topPlayerIdle = loadAnimation("assets/esqueleto1.png","assets/esqueleto1.png")  
+   bottomPlayerIdle = loadAnimation("assets/esqueletobaixo1.png","assets/esqueletobaixo1.png")
    topPlayerRunning = loadAnimation("assets/esqueleto1.png",
     "assets/esqueleto2.png",
     "assets/esqueleto3.png");
-  bottomPlayerRunning = loadAnimation("assets/esqueletobaixo1.png",
+   bottomPlayerRunning = loadAnimation("assets/esqueletobaixo1.png",
     "assets/esqueletobaixo2.png",
     "assets/esqueletobaixo3.png");
 
@@ -18,13 +19,13 @@ function preload() {
 
 function setup() {
   createCanvas(windowWidth,windowHeight);
- bg = createSprite(windowWidth/2,windowHeight/2);
- bg.addImage(bgImg);
- bg.scale = 3.2;
- topPlayer = createSprite(600,500);
- topPlayer.addAnimation("Running",topPlayerRunning);
- bottomPlayer = createSprite(400,500);
- bottomPlayer.addAnimation("Running",bottomPlayerRunning);
+  bg = createSprite(windowWidth/2,windowHeight/2);
+  bg.addImage(bgImg);
+  bg.scale = 3.2;
+  topPlayer = createSprite(600,500);
+  topPlayer.addAnimation("idle",topPlayerIdle);
+  bottomPlayer = createSprite(400,500);
+  bottomPlayer.addAnimation("idle",bottomPlayerIdle);
 }
 
 function draw() {
@@ -35,5 +36,4 @@ background(120);
   }
   drawSprites();
 }
-
 
