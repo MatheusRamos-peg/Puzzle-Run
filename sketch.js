197100e6f353b351c64e3237c2384a885cc3b187
JPoficial4 <joao.andrade.g4@gmail.com>
2026-04-20 21:00:43 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,7 +2,7 @@ var bg, bgImg;
 var topPlayer,bottomPlayer;
 var topPlayerIdle, bottomPlayerIdle,topPlayerImg,topPlayer,topPlayerRunning,bottomPlayerImg,bottomPlayerRunning;
 var lever, leverImg;
-var button, buttonImg; 
+var button, buttonImg,buttonTouched,buttonIdle1,buttonIdle2; 
 
 function preload() {
    bgImg = loadImage("assets/bg.avif");
@@ -14,7 +14,16 @@ function preload() {
    bottomPlayerRunning = loadAnimation("assets/esqueletobaixo1.png",
     "assets/esqueletobaixo2.png",
     "assets/esqueletobaixo3.png");
-
+   buttonTouched = loadAnimation("assets/Button_0.png",
+    "assets/Button_1.png",
+    "assets/Button_2.png");
+    buttonIdle1 = loadAnimation("assets/Button_0.png",
+    "assets/Button_0.png",
+    "assets/Button_0.png");
+    buttonIdle2= loadAnimation("assets/Button_2.png",
+    "assets/Button_2.png",
+    "assets/Button_2.png");
+   
 }
 
 function setup() {
@@ -23,16 +32,38 @@ function setup() {
   bg.addImage(bgImg);
   bg.scale = 3.2;
   topPlayer = createSprite(600,500);
-  topPlayer.addAnimation("idle",topPlayerIdle);
+  topPlayer.addAnimation("Idle",topPlayerIdle);
+  topPlayer.addAnimation("Running",topPlayerRunning);
   bottomPlayer = createSprite(400,500);
-  bottomPlayer.addAnimation("idle",bottomPlayerIdle);
+  bottomPlayer.addAnimation("Idle",bottomPlayerIdle);
+  bottomPlayer.addAnimation("Running",bottomPlayerRunning);
+  button = createSprite(200,500)
+  button.addAnimation("Idle1",buttonIdle1);
+  button.addAnimation("Touched",buttonTouched);
+  button.addAnimation("Idle2",buttonIdle2);
 }
 
 function draw() {
 background(120);
-  if(keyDown("x")){
+  if(keyDown("right")){
+    topPlayer.velocityX = 5;
     topPlayer.changeAnimation("Running");
+    topPlayer.mirrorX(1);
+  }
+  if(keyDown("left")){
+    topPlayer.velocityX = -5;
+    topPlayer.changeAnimation("Running");
+    topPlayer.mirrorX(-1);
+  }
+  if(keyDown("d")){
+    bottomPlayer.velocityX = 10
     bottomPlayer.changeAnimation("Running");
+    bottomPlayer.mirrorX(1);
+  };
+  if(keyDown("a")){
+    bottomPlayer.velocityX = -10;
+    bottomPlayer.changeAnimation("Running");
+    bottomPlayer.mirrorX(-1);
   }
   drawSprites();
 }
